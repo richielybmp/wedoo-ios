4fbdf6b1c486e5ee5b50da084d3f6a155621fe27
mstedler <mstedler@hotmail.com>
2019-04-06 14:39:08 -0300

    Adicionando Nav Controller

---
+++ WeDoo/TableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TableViewController.swift
+//  WeDoo
+//
+//  Created by Mateus Augusto Stedler on 06/04/19.
+//  Copyright © 2019 Richiely Paiva. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tvTableToDoo: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tvTableToDoo.dequeueReusableCell(withIdentifier: "toDooCell") as! ToDooCell
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addToDoo))
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func addToDoo() {
+        print("xablau")
+    }
+
+}
---
+++ WeDoo/ToDooCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDooCell.swift
+//  WeDoo
+//
+//  Created by Mateus Augusto Stedler on 06/04/19.
+//  Copyright © 2019 Richiely Paiva. All rights reserved.
+//
+
+import UIKit
+
+class ToDooCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
