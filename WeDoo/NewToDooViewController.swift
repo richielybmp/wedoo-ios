773cfaffa9ff5e3a0d1a1655161d6966050b01e1
mstedler <mstedler@hotmail.com>
2019-04-06 15:52:36 -0300

    NewToDoo Pop Stack

--- WeDoo/NewToDooViewController.swift
+++ WeDoo/NewToDooViewController.swift
@@ -27,4 +27,8 @@ class NewToDooViewController: UIViewController {
     }
     */
 
+    @IBAction func saveAndPop(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+        
+    }
 }
